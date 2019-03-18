df1ab60d4e564deb4a02af7b8930fb0572217e64
scytheofvyse <31118364+scytheofvyse@users.noreply.github.com>
2019-03-18 21:33:03 +0700

    Add files via upload

---
+++ day-3.js
@@ -0,0 +1,14 @@
+function showNumberTwo (){
+    return 2
+}
+
+var storedNumber = showNumberTwo ();
+    console.log(storedNumber)
+
+
+function addTwo (angka) {
+    return angka + 2
+}
+
+var x = addTwo(2);
+    console.log(x)
